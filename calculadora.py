e0b4c640d2e44ad5ba0b1cc7f2d6c23250558113
Esqueleto27 <matheofloresloor@gmail.com>
2025-02-06 13:05:14 -0500

    Modulo de operaciones

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,14 @@
 # Proyecto calculadora 
+
+#Creamos un modulo de operaciones 
+def suma(a,b):
+    return a + b
+
+def resta(a,b):
+    return a - b
+
+def multiplicacion(a,b):
+    return a * b
+
+def division(a,b):
+    return a / b
